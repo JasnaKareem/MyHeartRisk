9c5fdc1be5495c8e42b5b4d9d39c5241f191c444
JasnaKareem <Jesnakareem789@gmail.com>
2025-09-16 11:46:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,10 +9,10 @@ mainpath = os.path.dirname(__file__)
 heartimage = os.path.join(mainpath, r'heart.png')
 # Title
 st.image(heartimage, width=100)
-st.title('Heart Disease Predictor')
+st.title('MyHeartRisk')
 st.subheader('Standardized WebApp to Predict Heart Disease Based on Real World Hospital Case/Control Data')
 st.write('**Instruction**')
-st.write('This app uses machine learning to predict the likelihood of heart disease based on user-provided health parameters. Adjust the options in the sidebar to input your health data and click "Check" to see your risk assessment.')
+st.write('This app uses machine learning to predict the likelihood of heart disease based on user-provided health parameters. Adjust the options in the sidebar '>>' to input your health data and click "Check" to see your risk assessment.')
 # Load dataset
 tab1, tab2, tab3, tab4 = st.tabs(["Report", "Dataset", "Model", "Interpretation"])
 
@@ -66,3 +66,4 @@ with tab1:
 
         st.progress(int(confidence), text=f"Confidence Level: {confidence:.2f}%", width=400)
    
+
