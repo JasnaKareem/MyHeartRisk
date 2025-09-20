7efdf5f4816452ab6efac721d2a7ad12c71f9050
JasnaKareem <Jesnakareem789@gmail.com>
2025-09-20 17:29:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -300,6 +300,20 @@ model = joblib.load(model_path)
 
 # Show tabs and content only when analysis starts
 tab1, tab2, tab3, tab4 = st.tabs(["Report Dashboard", "Dataset", "Model Performance", "Feature Analysis"])
+with tab1:
+       st.markdown("""
+    <div style="text-align: center; padding: 1rem; background: linear-gradient(90deg, #ff6b6b, #ee5a6f); border-radius: 10px; margin-bottom: 1rem;">
+        <h2 style="color: white; margin: 0;">Welcome to MyHeartRisk</h2>
+        <p style="color: white; opacity: 0.9; margin: 0; margin-bottom: 2rem;">
+            Enter your health parameters in the sidebar to get started.
+        </p>
+        <div style="background: white; padding: 1.5rem; border-radius: 15px; margin: 1rem auto; max-width: 700px; box-shadow: 0 4px 6px rgba(0,0,0,0.1);">
+            <p style="color: #495057; margin: 0;">
+                📊 Interpretable Parameters • 🏥 Hospital-Grade Data • 🎯 94% Accuracy
+            </p>
+        </div>
+    </div>
+    """, unsafe_allow_html=True)
 if predict_btn:
   
     placeholder = st.empty()
@@ -392,20 +406,7 @@ if predict_btn:
    
    
 
-with tab1:
-       st.markdown("""
-    <div style="text-align: center; padding: 1rem; background: linear-gradient(90deg, #ff6b6b, #ee5a6f); border-radius: 10px; margin-bottom: 1rem;">
-        <h2 style="color: white; margin: 0;">Welcome to MyHeartRisk</h2>
-        <p style="color: white; opacity: 0.9; margin: 0; margin-bottom: 2rem;">
-            Enter your health parameters in the sidebar to get started.
-        </p>
-        <div style="background: white; padding: 1.5rem; border-radius: 15px; margin: 1rem auto; max-width: 700px; box-shadow: 0 4px 6px rgba(0,0,0,0.1);">
-            <p style="color: #495057; margin: 0;">
-                📊 Interpretable Parameters • 🏥 Hospital-Grade Data • 🎯 94% Accuracy
-            </p>
-        </div>
-    </div>
-    """, unsafe_allow_html=True)
+
     
 with tab2:
     st.markdown("""
@@ -545,6 +546,7 @@ with tab4:
     
     st.markdown("---")
     
+
 
 
 
